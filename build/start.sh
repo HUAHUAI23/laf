54f8eedd68c05e0079cea54b444cfd53baaa0cd2
HUAHUAI23 <huahua1319873800@outlook.com>
2023-12-27 02:59:59 +0000

    chore

--- build/start.sh
+++ build/start.sh
@@ -27,15 +27,15 @@ set -x
 
 DATABASE_URL="mongodb://${DB_USERNAME:-admin}:${PASSWD_OR_SECRET}@mongodb-0.mongo.${NAMESPACE}.svc.cluster.local:27017/sys_db?authSource=admin&replicaSet=rs0&w=majority"
 helm install mongodb -n ${NAMESPACE} \
-    --set db.username=${DB_USERNAME:-admin} \
-    --set db.password=${PASSWD_OR_SECRET} \
-    --set storage.size=${DB_PV_SIZE:-5Gi} \
-    ./charts/mongodb
+  --set db.username=${DB_USERNAME:-admin} \
+  --set db.password=${PASSWD_OR_SECRET} \
+  --set storage.size=${DB_PV_SIZE:-5Gi} \
+  ./charts/mongodb
 
 ## 3. install prometheus
 PROMETHEUS_URL=http://prometheus-laf.${NAMESPACE}.svc.cluster.local:9090
 if [ "$ENABLE_MONITOR" = "true" ]; then
-    kubectl apply -n ${NAMESPACE} -f <<EOF
+  kubectl apply -n ${NAMESPACE} -f - <<EOF
 apiVersion: monitoring.coreos.com/v1
 kind: Prometheus
 metadata:
@@ -61,7 +61,7 @@ spec:
     seccompProfile:
       type: RuntimeDefault
   evaluationInterval: 60s
-  image: quay.io/prometheus/prometheus:v2.45.0
+  image: quay.io/prometheus/prometheus:v2.42.0
   serviceMonitorSelector: {}
   probeSelector: {}
   ruleSelector: {}
@@ -99,12 +99,12 @@ spec:
   type: ClusterIP
 EOF
 
-    helm install prometheus-mongodb-exporter --version 3.2.0 -n ${NAMESPACE} \
-        --set mongodb.uri=${DATABASE_URL} \
-        --set serviceMonitor.enabled=true \
-        --set serviceMonitor.additionalLabels.release=prometheus \
-        --set serviceMonitor.additionalLabels.namespace=${NAMESPACE} \
-        ./charts/prometheus-mongodb-exporter
+  helm install prometheus-mongodb-exporter --version 3.2.0 -n ${NAMESPACE} \
+    --set mongodb.uri=${DATABASE_URL} \
+    --set serviceMonitor.enabled=true \
+    --set serviceMonitor.additionalLabels.release=prometheus \
+    --set serviceMonitor.additionalLabels.namespace=${NAMESPACE} \
+    ./charts/prometheus-mongodb-exporter
 fi
 
 ## 4. install minio
@@ -115,40 +115,40 @@ MINIO_EXTERNAL_ENDPOINT="${EXTERNAL_HTTP_SCHEMA}://${MINIO_DOMAIN}"
 MINIO_INTERNAL_ENDPOINT="${INTERNAL_HTTP_SCHEMA}://minio.${NAMESPACE}.svc.cluster.local:9000"
 
 helm install minio -n ${NAMESPACE} \
-    --set rootUser=${MINIO_ROOT_ACCESS_KEY} \
-    --set rootPassword=${MINIO_ROOT_SECRET_KEY} \
-    --set persistence.size=${OSS_PV_SIZE:-3Gi} \
-    --set domain=${MINIO_DOMAIN} \
-    --set consoleHost=${OSS_CONSOLE_DOMAIN} \
-    --set metrics.serviceMonitor.enabled=${ENABLE_MONITOR} \
-    --set metrics.serviceMonitor.additionalLabels.release=prometheus \
-    --set metrics.serviceMonitor.additionalLabels.namespace=${NAMESPACE} \
-    ./charts/minio
+  --set rootUser=${MINIO_ROOT_ACCESS_KEY} \
+  --set rootPassword=${MINIO_ROOT_SECRET_KEY} \
+  --set persistence.size=${OSS_PV_SIZE:-3Gi} \
+  --set domain=${MINIO_DOMAIN} \
+  --set consoleHost=${OSS_CONSOLE_DOMAIN} \
+  --set metrics.serviceMonitor.enabled=${ENABLE_MONITOR} \
+  --set metrics.serviceMonitor.additionalLabels.release=prometheus \
+  --set metrics.serviceMonitor.additionalLabels.namespace=${NAMESPACE} \
+  ./charts/minio
 
 ## 5. install laf-server
 SERVER_JWT_SECRET=$PASSWD_OR_SECRET
 RUNTIME_EXPORTER_SECRET=$PASSWD_OR_SECRET
 helm install server -n ${NAMESPACE} \
-    --set databaseUrl=${DATABASE_URL} \
-    --set jwt.secret=${SERVER_JWT_SECRET} \
-    --set apiServerHost=${API_DOMAIN} \
-    --set apiServerUrl=${EXTERNAL_HTTP_SCHEMA}://${API_DOMAIN} \
-    --set siteName=${WEB_DOMAIN} \
-    --set default_region.fixed_namespace=${NAMESPACE} \
-    --set default_region.database_url=${DATABASE_URL} \
-    --set default_region.minio_domain=${MINIO_DOMAIN} \
-    --set default_region.minio_external_endpoint=${MINIO_EXTERNAL_ENDPOINT} \
-    --set default_region.minio_internal_endpoint=${MINIO_INTERNAL_ENDPOINT} \
-    --set default_region.minio_root_access_key=${MINIO_ROOT_ACCESS_KEY} \
-    --set default_region.minio_root_secret_key=${MINIO_ROOT_SECRET_KEY} \
-    --set default_region.runtime_domain=${RUNTIME_DOMAIN} \
-    --set default_region.website_domain=${WEBSITE_DOMAIN} \
-    --set default_region.tls.enabled=true \
-    --set default_region.runtime_exporter_secret=${RUNTIME_EXPORTER_SECRET} \
-    $([ "$ENABLE_MONITOR" = "true" ] && echo "--set default_region.prometheus_url=${PROMETHEUS_URL}") \
-    ./charts/laf-server
+  --set databaseUrl=${DATABASE_URL} \
+  --set jwt.secret=${SERVER_JWT_SECRET} \
+  --set apiServerHost=${API_DOMAIN} \
+  --set apiServerUrl=${EXTERNAL_HTTP_SCHEMA}://${API_DOMAIN} \
+  --set siteName=${WEB_DOMAIN} \
+  --set default_region.fixed_namespace=${NAMESPACE} \
+  --set default_region.database_url=${DATABASE_URL} \
+  --set default_region.minio_domain=${MINIO_DOMAIN} \
+  --set default_region.minio_external_endpoint=${MINIO_EXTERNAL_ENDPOINT} \
+  --set default_region.minio_internal_endpoint=${MINIO_INTERNAL_ENDPOINT} \
+  --set default_region.minio_root_access_key=${MINIO_ROOT_ACCESS_KEY} \
+  --set default_region.minio_root_secret_key=${MINIO_ROOT_SECRET_KEY} \
+  --set default_region.runtime_domain=${RUNTIME_DOMAIN} \
+  --set default_region.website_domain=${WEBSITE_DOMAIN} \
+  --set default_region.tls.enabled=true \
+  --set default_region.runtime_exporter_secret=${RUNTIME_EXPORTER_SECRET} \
+  $([ "$ENABLE_MONITOR" = "true" ] && echo "--set default_region.prometheus_url=${PROMETHEUS_URL}") \
+  ./charts/laf-server
 
 ## 6. install laf-web
 helm install web -n ${NAMESPACE} \
-    --set domain=${WEB_DOMAIN} \
-    ./charts/laf-web
+  --set domain=${WEB_DOMAIN} \
+  ./charts/laf-web
